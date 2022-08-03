90dcceaef9c87b452f115204bb281097a712cb53
Eddie Osmanbasic <eddieosmanbasic@rocketmortgage.com>
2022-08-03 14:09:41 -0400

    uploaded lab 3, for week 1

---
+++ Lab1.3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+bool validNum = false;
+bool exitFlag = false;
+do
+{
+    Console.WriteLine("Hello! Please enter an integer.");
+    double input = double.Parse(Console.ReadLine());
+    if (input <= 0)
+    {
+        validNum = false;
+
+    }
+    else
+    {
+        validNum = true;
+        Console.WriteLine("Numbers\tSquared\tCubed");
+        Console.WriteLine("=====================");
+        for (double i = 1; i <= input; i++)
+        {
+            double squared = Math.Pow(i, 2);
+            double cubed = Math.Pow(i, 3);
+            Console.WriteLine($"{i}\t{squared}\t{cubed}");
+        }
+        Console.WriteLine("Would you like to go again? Please enter yes or no.");
+        string goAgain = Console.ReadLine();
+        if (goAgain != "yes")
+        {
+            exitFlag = true;
+
+        }
+        else
+        {
+            exitFlag = false;
+            validNum = false;
+
+        }
+
+    }
+
+    
+} while (!validNum && !exitFlag);
